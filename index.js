1e70afc6d6f622ad875a1ae35fe56190453e54ad
Baphomet-21 <76148986+Baphomet-21@users.noreply.github.com>
2020-12-17 10:15:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["link project"]
+const urls = ["https://github.com/Baphomet-21/uptimerobot/blob/master/index.js"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
